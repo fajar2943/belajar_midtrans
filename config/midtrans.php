b7478a89ba92c7ac5638d0ad5e1cea9cfd5a0b3a
Fajar Lintang Gumilang <fajar2943@gmail.com>
2023-03-10 11:45:45 +0700

    snap url

--- config/midtrans.php
+++ config/midtrans.php
@@ -5,4 +5,5 @@ return [
     'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'server_key' => env('MIDTRANS_SERVER_KEY'),
     'is_production' => env('MIDTRANS_IS_PRODUCTION'),
+    'snap_url' => env('MIDTRANS_SNAP_URL'),
 ];
--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- @TODO: replace SET_YOUR_CLIENT_KEY_HERE with your client key -->
-    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
+    <script type="text/javascript" src="{{config('midrans.snap_url')}}"
         data-client-key="{{config('midtrans.client_key')}}"></script>
     <!-- Note: replace with src="https://app.midtrans.com/snap/snap.js" for Production environment -->
     <title>Toko Durian</title>
